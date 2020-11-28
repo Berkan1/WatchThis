28a143f3196467bab6ae0b257b8e9a50d70756b2
Berkan1 <berkan.c@hotmail.com>
2020-11-28 00:54:20 +0000

    Added nav links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from "./components/nav-bar";
+import { BrowserRouter, HashRouter, Switch, Route, Link } from 'react-router-dom';
+import Profile from '../src/components/profile';
+import Test from '../src/components/test';
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from '../src/components/login-button';
+import LogoutButton from '../src/components/logout-button';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <div className="App">
-      <NavBar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main-body">
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Switch>
+              <Route exact path='/' component={Test} />
+              <Route exact path='/profile' component={Profile} />
+            </Switch>
+            <Link to="/">Home</Link>
+            <Link to="/profile">Profile</Link>
+            <Nav className="justify-content-end">
+          {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+      </Nav>
+        </BrowserRouter>
+      </div>
+      <footer className="footer">
+        This site was created for educational purposes using the API provided by <a href="https://www.thecocktaildb.com/api.php">TheCocktailDB</a>. Please support their site!
+      </footer>
     </div>
   );
 }
---
+++ src/components/test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export const Test = () => {
+  return (
+    <Container className="mb-5">
+        Hellooo
+      
+    </Container>
+  );
+};
+
+export default Test;
