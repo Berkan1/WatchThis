d958e49374f080d1eee5de1b7cc486702bb07e63
Berkan1 <berkan.c@hotmail.com>
2020-12-05 00:23:43 +0000

    Preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,12 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static('../build'));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
@@ -20,6 +23,10 @@ connection.once('open', () => {
 const filmsRouter = require('./routes/films');
 
 app.use('/films', filmsRouter);
+
+app.use(function(req, res) {
+	res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
